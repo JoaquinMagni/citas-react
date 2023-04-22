47878e195abdcf62c45138984a0d182eac0ebebf
Joaquin <joaquinagueromagni@gmail.com>
2023-04-22 09:13:46 -0300

    Primer Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return ( 
+        <>  
+        <h1 className="text-7xl text-center md:w-2/3 mx-auto font-bold my-5">
+            Veterinaria {""}
+            <span className="text-blue-700">"Stylo Pet"</span>
+        </h1>
+
+        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+            Registro de Pacientes {""}
+        </h1>   
+        </>   
+    )
+
+}
+
+export default Header;
