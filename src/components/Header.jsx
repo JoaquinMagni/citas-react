c07dd0a1da8f546b9991c03507ed24cad1d6321c
Joaquin <joaquinagueromagni@gmail.com>
2023-04-22 09:27:39 -0300

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         </h1>
 
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Registro de Pacientes {""}
+            Registro de Pacientes {""} Con Github {''}
         </h1>   
         </>   
     )
